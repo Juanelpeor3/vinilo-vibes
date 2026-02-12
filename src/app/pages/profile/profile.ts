a49511da2fdc5749da625b63f236552b1bab6917
Juanelpeor3 <juan.mingo.1911@gmail.com>
2026-02-12 09:53:10 +0100

    add spinner to profile page

--- src/app/pages/profile/profile.ts
+++ src/app/pages/profile/profile.ts
@@ -7,10 +7,11 @@ import { MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle } 
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { ProfileService } from '../../services/profile/profile';
+import { MatProgressSpinner } from "@angular/material/progress-spinner";
 
 @Component({
   selector: 'app-profile',
-  imports: [CommonModule, MatButtonModule, MatCard, MatCardHeader, MatCardContent, MatCardActions, MatCardTitle, MatIconModule, RouterModule, MatMenuModule],
+  imports: [CommonModule, MatButtonModule, MatCard, MatCardHeader, MatCardContent, MatCardActions, MatCardTitle, MatIconModule, RouterModule, MatMenuModule, MatProgressSpinner],
   templateUrl: './profile.html',
   styleUrl: './profile.scss',
 })
@@ -18,12 +19,16 @@ export class Profile {
   constructor(private router: Router, private profileService: ProfileService) { }
   profile = signal<any>(null);
 
+    isLoading = signal(true);
+
   async ngOnInit() {
+
     const { data } = await this.profileService.getProfile();
 
     if (data) {
       // Actualiza el signal
       this.profile.set(data);
+      this.isLoading.set(false);
     }
   }
 
