3071d6daba88c69882c4f8ae476006789cdfd0f7
Juanelpeor3 <juan.mingo.1911@gmail.com>
2026-02-12 23:42:34 +0100

    handle not found state in vinyl list

--- src/app/shared/components/vinyl-list/vinyl-list.ts
+++ src/app/shared/components/vinyl-list/vinyl-list.ts
@@ -6,10 +6,11 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { ActivatedRoute } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 import { Vinyl } from '../../models/vinyl-model';
+import { PageNotFound } from "../../../pages/page-not-found/page-not-found";
 
 @Component({
   selector: 'app-vinyl-list',
-  imports: [CommonModule, VinylCard, MatProgressSpinnerModule],
+  imports: [CommonModule, VinylCard, MatProgressSpinnerModule, PageNotFound],
   templateUrl: './vinyl-list.html',
   styleUrl: './vinyl-list.scss',
 })
@@ -27,12 +28,17 @@ export class VinylList {
 
   vinyls = signal<Vinyl[]>([]);
   isLoading = signal(true);
-  currentName = signal<string>("Todos los Vinilos disponibles:");
+  notFound = signal(false);
 
+  currentName = signal<string>("");
   async ngOnInit() {
     const genreId = this.route.snapshot.paramMap.get('genreId');
     // Si hay un genreId, obtenemos los vinilos de ese género específico, de lo contrario, obtenemos todos los vinilos
     if (genreId) {
+      if (genreId && !this.genre_names[genreId]) {
+        this.notFound.set(true); // O a una página de error
+        return;
+      }
       // Con el genreId, obtenemos los vinilos de ese género específico
       this.titleService.setTitle(`${this.genre_names[genreId]} | Vinilo Vibes`);
       this.currentName.set(`Vinilos de ${this.genre_names[genreId]}:`);
@@ -43,6 +49,7 @@ export class VinylList {
     else {
       this.vinyls.set(await this.vinylService.getAll());
       this.isLoading.set(false);
+      this.currentName.set("Todos los Vinilos disponibles:");
     }
   }
 }
