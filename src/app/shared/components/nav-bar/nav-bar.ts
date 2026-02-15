63230628ec09b1674478c0d0fba9c9e693c061c2
Juanelpeor3 <juan.mingo.1911@gmail.com>
2026-02-15 01:34:26 +0100

    implement CartService and change icons in nav-bar component

--- src/app/shared/components/nav-bar/nav-bar.ts
+++ src/app/shared/components/nav-bar/nav-bar.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CartService } from '../../../services/cart/cart';
+import { MatIcon } from "@angular/material/icon";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
+  imports: [MatIcon, RouterLink],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.scss',
 })
 export class NavBar {
-
+  cartService = inject(CartService);
 }
