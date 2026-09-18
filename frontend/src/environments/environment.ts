8800fc83923d0e334a93a6faff8086a05161c049
Juanelpeor3 <juan.mingo.1911@gmail.com>
2026-09-18 17:45:49 +0200

    feat(frontend): add HTTP interceptor and configure HttpClient

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: false,
-    supabaseUrl: "https://wefyftvjysetzabvuttv.supabase.co",
-    supabaseAnonKey: "sb_publishable_URwrm2MX5qicUpMuq7MXdQ_B4UFo7Nw"
+    apiUrl: 'http://localhost:5196/api'
 };
